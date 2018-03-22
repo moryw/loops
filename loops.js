9b73a25367c1b0722b1d873a25f737e4ec33bd43
moryw <matt@skitskat.xyz>
2018-03-22 12:22:06 -0400

    Add cookies for loop.

--- loops.js
+++ loops.js
@@ -1,1 +1,6 @@
 //this is to practice with loops
+let cookies = ['chocolate chip', 'raisin', 'macadamia nut', 'sugar'];
+
+for (let i = 0; i<cookies.length; i++) {
+  console.log('I would love to eat a ' + cookies[i] + ' cookie right now!');
+}
