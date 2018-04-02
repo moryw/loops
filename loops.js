7afe4a58eaad57c3ceefd33cfb547b8f7ae7be21
moryw <matt@skitskat.xyz>
2018-04-02 12:20:44 -0300

    Add two arrays for places to compare.

--- loops.js
+++ loops.js
@@ -1,12 +1,19 @@
-//this is to practice with loops
-let cookies = ['chocolate chip', 'raisin', 'macadamia nut', 'sugar'];
+// //this is to practice with loops
+// let cookies = ['chocolate chip', 'raisin', 'macadamia nut', 'sugar'];
+//
+// for (let i = 0; i<cookies.length; i++) {
+//   //console.log('I would love to eat a ' + cookies[i] + ' cookie right now!');
+// }
+//
+// const vacationSpots = ['India', 'UK', 'Scotland'];
+//
+// for (let vacationSpotIndex = vacationSpots.length - 1; vacationSpotIndex >= 0; vacationSpotIndex--) {
+//   console.log(`I would like to visit: ${vacationSpots[vacationSpotIndex]}`);
+// }
 
-for (let i = 0; i<cookies.length; i++) {
-  //console.log('I would love to eat a ' + cookies[i] + ' cookie right now!');
-}
 
-const vacationSpots = ['India', 'UK', 'Scotland'];
+//to loop to compare to arrays
 
-for (let vacationSpotIndex = vacationSpots.length - 1; vacationSpotIndex >= 0; vacationSpotIndex--) {
-  console.log(`I would like to visit: ${vacationSpots[vacationSpotIndex]}`);
-}
+let myPlaces = ['India', 'Scotland', 'Europe'];
+
+let friendPlaces = ['India', 'Napol', 'Africa'];
